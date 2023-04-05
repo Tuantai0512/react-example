1cf2ff70b7530342812779ca61812b0f86f22492
dev-peter <tuantai0512@gmail.com>
2023-04-05 21:41:15 +0700

    Adding button back in blog detail

--- src/wiews/BlogDetail/index.js
+++ src/wiews/BlogDetail/index.js
@@ -1,18 +1,23 @@
 import "../BlogDetail/style.scss";
 import useFetch from "../../services/fetchData";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 function BlogDetail() {
     const { id} = useParams();
+    const nav = useNavigate();
     const { data: dataBlog, isError, loading } = useFetch('https://jsonplaceholder.typicode.com/posts', false);
     let newData = [];
     if (dataBlog && dataBlog.length > 0) {
         newData = dataBlog.slice(0, 9);
     }
     console.log(newData);
+    const handleBack = () => {
+        nav("/blog");
+    }
 
     return (
         <>
+            <button onClick={handleBack}>&lt;--  Back</button>
             <h2>This is blog Detail page by id = {id }</h2>
         </>
     )
