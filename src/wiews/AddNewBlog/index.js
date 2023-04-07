f71327a4a4233e6d24ed04fc94a0f943964619d6
dev-peter <tuantai0512@gmail.com>
2023-04-07 22:24:11 +0700

    Create validate form add blog page

--- src/wiews/AddNewBlog/index.js
+++ src/wiews/AddNewBlog/index.js
@@ -4,21 +4,32 @@ import { useState } from "react";
 function AddNewBlog() {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(!title){
+            alert('empty title!');
+            return
+        }
+        if(!content){
+            alert('empty content!');
+            return
+        }
         console.log("Check form:", title, " + ", content);
     }
     return (
         <div className="add-new-container">
-            <h2 className="text-add-new">Add new blog</h2>
-            <div className="inputs-data">
-                <label>Title</label>
-                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}></input>
-            </div>
-            <div className="inputs-data">
-                <label>Content</label>
-                <input type="text" value={content} onChange={(e) => setContent(e.target.value)}></input>
-            </div>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
+            <form onSubmit={handleSubmit}>
+                <h2 className="text-add-new">Add new blog</h2>
+                <div className="inputs-data">
+                    <label>Title</label>
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+                </div>
+                <div className="inputs-data">
+                    <label>Content</label>
+                    <input type="text" value={content} onChange={(e) => setContent(e.target.value)}></input>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
